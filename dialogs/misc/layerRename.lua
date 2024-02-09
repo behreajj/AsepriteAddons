e9738f5d0671f3eb49e17886f0116a702d8cd097
Jeremy Behreandt <behreandtjeremy@gmail.com>
2024-02-08 21:57:27 -0600

    Layer rename count all layers.

--- dialogs/misc/layerRename.lua
+++ dialogs/misc/layerRename.lua
@@ -16,6 +16,47 @@ dlg:check {
     selected = false
 }
 
+---@param layer Layer
+---@param t Layer[]
+---@param u table<integer, integer>
+---@return Layer[]
+---@return table<integer, integer>
+local function layerHierarchy(layer, t, u)
+    if layer.isGroup then
+        local children <const> = layer.layers
+        if children then
+            local lenChildren <const> = #children
+            local i = 0
+            while i < lenChildren do
+                i = i + 1
+                layerHierarchy(children[i], t, u)
+            end
+        end
+    end
+
+    t[#t + 1] = layer
+    u[layer.id] = #t
+    return t, u
+end
+
+---@param sprite Sprite
+---@return Layer[]
+---@return table<integer, integer>
+local function spriteHierarchy(sprite)
+    ---@type Layer[]
+    local t <const> = {}
+    ---@type table<integer, integer>
+    local u <const> = {}
+    local topLayers <const> = sprite.layers
+    local lenTopLayers <const> = #topLayers
+    local h = 0
+    while h < lenTopLayers do
+        h = h + 1
+        layerHierarchy(topLayers[h], t, u)
+    end
+    return t, u
+end
+
 dlg:button {
     id = "confirm",
     text = "&OK",
@@ -24,66 +65,52 @@ dlg:button {
         local sprite <const> = app.sprite
         if not sprite then return end
 
-        local tlHidden <const> = not app.preferences.general.visible_timeline
-        if tlHidden then
-            app.command.Timeline { open = true }
-        end
-
         local range <const> = app.range
-        if range.sprite == sprite then
-            local rangeLayers <const> = range.layers
-            local lenRangeLayers <const> = #rangeLayers
-            if lenRangeLayers > 0 then
-                ---@type Layer[]
-                local sortedLayers <const> = {}
-                local h = 0
-                while h < lenRangeLayers do
-                    -- Exclude reference or background?
-                    h = h + 1
-                    sortedLayers[h] = rangeLayers[h]
-                end
+        if range.sprite ~= sprite then return end
 
-                -- TODO: Exclude empty name entry?
-                local args <const> = dlg.data
-                local nameEntry <const> = args.nameEntry --[[@as string]]
-                local reverse <const> = args.reverse --[[@as boolean]]
+        local rangeLayers <const> = range.layers
+        local lenRangeLayers <const> = #rangeLayers
 
-                local lenSortedLayers <const> = #sortedLayers
-                if lenSortedLayers == 1 then
-                    app.transaction("Rename Layer", function()
-                        sortedLayers[1].name = nameEntry
-                    end)
-                else
-                    table.sort(sortedLayers, function(a, b)
-                        local apid <const> = a.parent.id
-                        local bpid <const> = b.parent.id
-                        if apid == bpid then
-                            return a.stackIndex < b.stackIndex
-                        end
-                        return apid < bpid
-                    end)
+        if lenRangeLayers <= 0 then return end
 
-                    local format <const> = "%s %d"
-                    local strfmt <const> = string.format
+        local args <const> = dlg.data
+        local nameEntry <const> = args.nameEntry --[[@as string]]
+        local reverse <const> = args.reverse --[[@as boolean]]
 
-                    app.transaction("Rename Layers", function()
-                        local i = 0
-                        while i < lenSortedLayers do
-                            i = i + 1
-                            local layer <const> = sortedLayers[i]
-                            local n <const> = reverse
-                                and lenSortedLayers + 1 - i
-                                or i
-                            layer.name = strfmt(format, nameEntry, n)
-                        end
-                    end)
-                end
+        if lenRangeLayers <= 1 then
+            app.transaction("Rename Layer", function()
+                rangeLayers[1].name = nameEntry
+            end)
+            app.refresh()
+            return
+        end
+
+        local t <const>, u <const> = spriteHierarchy(sprite)
+
+        ---@type Layer[]
+        local sortedLayers <const> = {}
+        local j = 0
+        while j < lenRangeLayers do
+            j = j + 1
+            sortedLayers[j] = rangeLayers[j]
+        end
+        table.sort(sortedLayers, function(a, b) return u[a.id] < u[b.id] end)
+
+        local format <const> = "%s %d"
+        local strfmt <const> = string.format
+        local lenSortedLayers <const> = #sortedLayers
+
+        app.transaction("Rename Layers", function()
+            local i = 0
+            while i < lenSortedLayers do
+                i = i + 1
+                local layer <const> = sortedLayers[i]
+                local n <const> = reverse
+                    and lenSortedLayers + 1 - i
+                    or i
+                layer.name = strfmt(format, nameEntry, n)
             end
-        end
-
-        if tlHidden then
-            app.command.Timeline { close = true }
-        end
+        end)
 
         app.refresh()
     end
