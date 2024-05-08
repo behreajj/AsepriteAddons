e7bcc4704271c3245d8174ab33e05d171dccfd59
Jeremy Behreandt <behreandtjeremy@gmail.com>
2024-05-08 13:32:13 -0500

    Toggle visible unhide timeline.

--- commands/toggleVisible.lua
+++ commands/toggleVisible.lua
@@ -1,6 +1,11 @@
 local sprite <const> = app.sprite
 if not sprite then return end
 
+local tlHidden <const> = not app.preferences.general.visible_timeline --[[@as boolean]]
+if tlHidden then
+    app.command.Timeline { open = true }
+end
+
 local range <const> = app.range
 if range.sprite == sprite then
     local rangeType <const> = range.type
@@ -21,6 +26,10 @@ if range.sprite == sprite then
             layer.isVisible = not layer.isVisible
         end
     end
+end
+
+if tlHidden then
+    app.command.Timeline { close = true }
 end
 
 app.refresh()
