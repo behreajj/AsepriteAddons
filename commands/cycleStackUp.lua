db323f429944a7e74515109e85f82624be65a401
Jeremy Behreandt <behreandtjeremy@gmail.com>
2024-05-17 21:53:26 -0500

    Cycle stack up and down Remove type annotation.

--- commands/cycleStackUp.lua
+++ commands/cycleStackUp.lua
@@ -8,22 +8,24 @@ if layer.isBackground then return end
 local shift <const> = 1
 
 local parent <const> = layer.parent
-local parentLayers <const> = parent.layers --[=[@as Layer[]]=]
-local lenNeighbors <const> = #parentLayers
+local parentLayers <const> = parent.layers
+if parentLayers then
+    local lenNeighbors <const> = #parentLayers
 
-if lenNeighbors > 1 then
-    -- Edge case where background layer is at the bottom of the stack.
-    -- A background can still be moved into a group. Sprite:backgroundLayer
-    -- is unreliable here.
-    local currStackIndex <const> = layer.stackIndex
-    if currStackIndex == lenNeighbors
-        and parentLayers[1].isBackground then
-        return
+    if lenNeighbors > 1 then
+        -- Edge case where background layer is at the bottom of the stack.
+        -- A background can still be moved into a group. Sprite:backgroundLayer
+        -- is unreliable here.
+        local currStackIndex <const> = layer.stackIndex
+        if currStackIndex == lenNeighbors
+            and parentLayers[1].isBackground then
+            return
+        end
+
+        app.transaction("Cycle Stack Up", function()
+            local shifted <const> = currStackIndex - 1 + shift
+            layer.stackIndex = 1 + shifted % lenNeighbors
+            app.layer = layer
+        end)
     end
-
-    app.transaction("Cycle Stack Up", function()
-        local shifted <const> = currStackIndex - 1 + shift
-        layer.stackIndex = 1 + shifted % lenNeighbors
-        app.layer = layer
-    end)
 end
