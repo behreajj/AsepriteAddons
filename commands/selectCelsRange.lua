86f27d12a27a157cf4b2dfd302059eeae3f6fd85
Jeremy Behreandt <behreandtjeremy@gmail.com>
2024-07-10 21:06:24 -0500

    Select Cels Select sprite bounds when layer is background. Intersect selection with sprite bounds.

--- commands/selectCelsRange.lua
+++ commands/selectCelsRange.lua
@@ -12,30 +12,36 @@ end
 local appRange <const> = app.range
 if appRange.sprite == activeSprite then
     local selectCel <const> = AseUtilities.selectCel
+    local spriteBounds <const> = activeSprite.bounds
 
     if appRange.isEmpty then
         local activeLayer <const> = site.layer
         local activeFrame <const> = site.frame
         if activeLayer and activeFrame then
-            local leaves <const> = AseUtilities.appendLeaves(
-                activeLayer, {}, true, true, true, true)
-            local lenLeaves <const> = #leaves
-            local union <const> = Selection()
+            if activeLayer.isBackground then
+                activeSprite.selection = Selection(spriteBounds)
+            else
+                local leaves <const> = AseUtilities.appendLeaves(
+                    activeLayer, {}, true, true, true, true)
+                local lenLeaves <const> = #leaves
+                local union <const> = Selection()
 
-            local i = 0
-            while i < lenLeaves do
-                i = i + 1
-                local leaf <const> = leaves[i]
-                local cel <const> = leaf:cel(activeFrame)
-                if cel then
-                    union:add(selectCel(cel))
+                local i = 0
+                while i < lenLeaves do
+                    i = i + 1
+                    local leaf <const> = leaves[i]
+                    local cel <const> = leaf:cel(activeFrame)
+                    if cel then
+                        union:add(selectCel(cel))
+                    end
                 end
-            end
 
-            if not union.isEmpty then
-                activeSprite.selection = union
-            end
-        end
+                union:intersect(spriteBounds)
+                if not union.isEmpty then
+                    activeSprite.selection = union
+                end -- Non empty union.
+            end     -- Layer is background check.
+        end         -- Active Layer and frame exist.
     else
         local images <const> = appRange.images
         local lenImages <const> = #images
@@ -47,16 +53,20 @@ if appRange.sprite == activeSprite then
             local image <const> = images[i]
             local cel <const> = image.cel
             local layer <const> = cel.layer
-            if not layer.isReference then
+            if layer.isBackground then
+                union:add(spriteBounds)
+                break
+            elseif not layer.isReference then
                 union:add(selectCel(cel))
             end
         end
 
+        union:intersect(spriteBounds)
         if not union.isEmpty then
             activeSprite.selection = union
-        end
-    end
-end
+        end -- Non empty union.
+    end     -- Range is empty check.
+end         -- Range sprite is active sprite.
 
 if tlHidden then
     app.command.Timeline { close = true }
