e72a4516e4d514a323fd9debfb9a600fc3c1a241
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-01-14 02:42:34 -0600

    AseUtilities changePixelFormat Set CIE LAB preference via preferences.

--- support/aseutilities.lua
+++ support/aseutilities.lua
@@ -1077,12 +1077,18 @@ end
 ---@param format ColorMode format constant
 function AseUtilities.changePixelFormat(format)
     if format == ColorMode.INDEXED then
-        -- TODO: For now, CIE LAB preference would need to be set through
-        -- preferences.
+        -- For now, CIE LAB preference has to be set in preferences.
         -- https://github.com/aseprite/aseprite/pull/4925
         -- https://github.com/aseprite/aseprite/blob/main/src/doc/fit_criteria.h
         -- https://github.com/aseprite/aseprite/blob/main/data/pref.xml#L324
-        app.command.ChangePixelFormat { format = "indexed" }
+        local oldFit <const> = app.preferences.quantization.fit_criteria --[[@as integer]]
+        app.preferences.quantization.fit_criteria = 4
+        app.command.ChangePixelFormat {
+            format = "indexed",
+            rgbmap = "octree",
+            dithering = "error-diffusion",
+        }
+        app.preferences.quantization.fit_criteria = oldFit
     elseif format == ColorMode.GRAY then
         app.command.ChangePixelFormat { format = "gray" }
     elseif format == ColorMode.RGB then
