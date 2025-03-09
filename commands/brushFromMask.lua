10f85302f0992c1a81e43ff9e112fac44ba2af00
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-03-08 19:31:20 -0600

    Brush From Mask Do not set brush center to top left if cursor snap is enabled.

--- commands/brushFromMask.lua
+++ commands/brushFromMask.lua
@@ -12,26 +12,42 @@ local image <const>, xSel <const>, ySel <const> = AseUtilities.selToImage(
     sel, sprite, frame.frameNumber)
 
 local appPrefs <const> = app.preferences
-local useSnap = false
+local useGridSnap = false
+local cursorSnap = false
 if appPrefs then
+    local cursorPrefs <const> = appPrefs.cursor
+    if cursorPrefs then
+        local snapToGrid <const> = cursorPrefs.snap_to_grid
+        if snapToGrid then
+            cursorSnap = true
+        end
+    end
+
     local docPrefs <const> = appPrefs.document(sprite)
     if docPrefs then
         local gridPrefs <const> = docPrefs.grid
         if gridPrefs then
             local snapPref <const> = gridPrefs.snap --[[@as boolean]]
             if snapPref then
-                useSnap = snapPref
-            end
-        end
-    end
-end
+                useGridSnap = snapPref
+            end -- End snap prefs exists.
+        end     -- End grid prefs exists.
+    end         -- End doc prefs exists.
+end             -- End preferences exists.
 
 -- Ideally, this would also turn off strict tile alignment mode,
 -- but unsure how to do this, as there's only the command to toggle,
 -- not a preference for the document's current state.
 local brushPattern = BrushPattern.NONE
-if site.layer and site.layer.isTilemap then
-    if useSnap then
+
+-- You could change these to tile map top left corner, but the problem is
+-- when the selection size doesn't match up to tile dimensions.
+local xPattern = xSel
+local yPattern = ySel
+
+local activeLayer <const> = site.layer
+if activeLayer and activeLayer.isTilemap then
+    if useGridSnap then
         brushPattern = BrushPattern.TARGET
     else
         brushPattern = BrushPattern.ORIGIN
@@ -39,25 +55,23 @@ if site.layer and site.layer.isTilemap then
 end
 
 local centerPreset = "CENTER"
-if useSnap then
+if (not cursorSnap) and useGridSnap then
     centerPreset = "TOP_LEFT"
-else
-    if appPrefs then
-        local maskPrefs <const> = appPrefs.selection
-        if maskPrefs then
-            -- https://github.com/aseprite/aseprite/blob/main/data/pref.xml#L81
-            local maskPivot <const> = maskPrefs.pivot_position --[[@as integer]]
-            if maskPivot then
-                local centerPresets <const> = {
-                    "TOP_LEFT", "TOP_CENTER", "TOP_RIGHT",
-                    "CENTER_LEFT", "CENTER", "CENTER_RIGHT",
-                    "BOTTOM_LEFT", "BOTTOM_CENTER", "BOTTOM_RIGHT"
-                }
-                centerPreset = centerPresets[1 + maskPivot % #centerPresets]
-            end -- Mask pivot exists.
-        end     -- Mask preferences exists.
-    end         -- App preferencs exists.
-end             -- Use snap check.
+elseif appPrefs then
+    -- https://github.com/aseprite/aseprite/blob/main/data/pref.xml#L81
+    local maskPrefs <const> = appPrefs.selection
+    if maskPrefs then
+        local maskPivot <const> = maskPrefs.pivot_position --[[@as integer]]
+        if maskPivot then
+            local centerPresets <const> = {
+                "TOP_LEFT", "TOP_CENTER", "TOP_RIGHT",
+                "CENTER_LEFT", "CENTER", "CENTER_RIGHT",
+                "BOTTOM_LEFT", "BOTTOM_CENTER", "BOTTOM_RIGHT"
+            }
+            centerPreset = centerPresets[1 + maskPivot % #centerPresets]
+        end     -- Mask pivot exists.
+    end         -- Mask preferences exists.
+end             -- Use grid snap check.
 
 app.transaction("Brush From Mask", function()
     if appPrefs then
@@ -69,7 +83,7 @@ app.transaction("Brush From Mask", function()
 
     sprite.selection:deselect()
     app.brush = AseUtilities.imageToBrush(
-        image, centerPreset, brushPattern, xSel, ySel)
+        image, centerPreset, brushPattern, xPattern, yPattern)
     app.tool = "pencil"
 end)
 
