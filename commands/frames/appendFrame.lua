bac702456567c394bc7b2a0e17229705d4b207b6
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-04-29 11:50:18 -0500

    Append and prepend frames Include comment about active tag behavior.

--- commands/frames/appendFrame.lua
+++ commands/frames/appendFrame.lua
@@ -1,3 +1,7 @@
+-- The default behavior for the active tag, if any, is to
+-- expand to encompass this frame. That seems desirable
+-- for this case (?), but it may not be in others.
+
 local site <const> = app.site
 local activeSprite <const> = site.sprite
 if not activeSprite then return end
