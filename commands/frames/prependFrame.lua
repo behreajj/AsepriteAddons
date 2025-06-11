af547163f3c2d88f9b3c5564668cc55498d65185
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-06-11 00:36:19 -0500

    Wrap append frame and prepend frame in a named transaction.

--- commands/frames/prependFrame.lua
+++ commands/frames/prependFrame.lua
@@ -6,11 +6,13 @@ local site <const> = app.site
 local activeSprite <const> = site.sprite
 if not activeSprite then return end
 
-local oldActiveFrObj <const> = site.frame
-if oldActiveFrObj then
-    local oldActiveFrIdx <const> = oldActiveFrObj.frameNumber
-    activeSprite:newEmptyFrame(oldActiveFrIdx)
-    app.frame = activeSprite.frames[oldActiveFrIdx + 1]
-else
-    activeSprite:newEmptyFrame()
-end
+app.transaction("Prepend Frame", function()
+    local oldActiveFrObj <const> = site.frame
+    if oldActiveFrObj then
+        local oldActiveFrIdx <const> = oldActiveFrObj.frameNumber
+        activeSprite:newEmptyFrame(oldActiveFrIdx)
+        app.frame = activeSprite.frames[oldActiveFrIdx + 1]
+    else
+        activeSprite:newEmptyFrame()
+    end
+end)
