7d94381f7180ad1457b9bb7f0e9fca86a1674f2f
Jeremy Behreandt <behreandtjeremy@gmail.com>
2025-06-26 22:56:55 -0500

    Palette subset dialog Initial draft.

---
+++ dialogs/palette/paletteSubset.lua
@@ -0,0 +1,157 @@
+dofile("../../support/aseutilities.lua")
+
+local defaults <const> = {
+    rangeStr = "",
+    strExample = "4,6:9,13",
+    uniquesOnly = false,
+    prependMask = true,
+}
+
+local dlg <const> = Dialog { title = "Palette Subset" }
+
+dlg:newrow { always = false }
+
+dlg:check {
+    id = "uniquesOnly",
+    label = "Filter:",
+    text = "Uniques",
+    selected = defaults.uniquesOnly,
+    hexpand = false,
+    focus = false,
+}
+
+dlg:newrow { always = false }
+
+dlg:check {
+    id = "prependMask",
+    label = "Mask:",
+    text = "Prepend",
+    selected = defaults.prependMask,
+    hexpand = false,
+    focus = false,
+}
+
+dlg:newrow { always = false }
+
+dlg:button {
+    id = "confirm",
+    text = "&OK",
+    focus = true,
+    onclick = function()
+        local site <const> = app.site
+        local activeSprite <const> = site.sprite
+        if not activeSprite then
+            app.alert {
+                title = "Error",
+                text = "There is no active sprite."
+            }
+            return
+        end
+
+        local activeFrame <const> = site.frame
+        if not activeFrame then
+            app.alert {
+                title = "Error",
+                text = "There is no active frame."
+            }
+            return
+        end
+
+        local spec <const> = activeSprite.spec
+        local colorMode <const> = spec.colorMode
+        if colorMode ~= ColorMode.RGB then
+            app.alert {
+                title = "Error",
+                text = "Only RGB color mode is supported."
+            }
+            return
+        end
+
+        -- TODO: See cycle indices, could use a range string...
+        local chosenIdcs <const> = {}
+
+        local range <const> = app.range
+        local rangeIsValid <const> = range.sprite == activeSprite
+        if rangeIsValid then
+            local rangeColors <const> = range.colors
+            local lenRangeColors <const> = #rangeColors
+            local h = 0
+            while h < lenRangeColors do
+                h = h + 1
+                chosenIdcs[h] = rangeColors[h]
+            end
+        end
+
+        local lenChosenIdcs <const> = #chosenIdcs
+        if lenChosenIdcs <= 0 then
+            app.alert {
+                title = "Error",
+                text = "No swatches were chosen."
+            }
+            return
+        end
+
+        local palettes <const> = activeSprite.palettes
+        local palette <const> = AseUtilities.getPalette(activeFrame, palettes)
+        local lenPalette <const> = #palette
+
+        if lenPalette <= 1 then
+            app.alert {
+                title = "Error",
+                text = "Palette has only one color."
+            }
+            return
+        end
+
+        ---@type integer[]
+        local abgr32s = {}
+        local i = 0
+        while i < lenChosenIdcs do
+            i = i + 1
+            local idx <const> = chosenIdcs[i]
+            local aseColor <const> = palette:getColor(idx)
+            abgr32s[i] = AseUtilities.aseColorToHex(aseColor, ColorMode.RGB)
+        end
+
+        local args <const> = dlg.data
+        local prependMask <const> = args.prependMask --[[@as boolean]]
+        local uniquesOnly <const> = args.uniquesOnly --[[@as boolean]]
+
+        if uniquesOnly then
+            local abgr32sUnique <const>,
+            _ <const> = Utilities.uniqueColors(abgr32s, true)
+            abgr32s = abgr32sUnique
+        end
+
+        if prependMask then
+            Utilities.prependMask(abgr32s)
+        end
+
+        app.transaction("Palette Subset", function()
+            local lenAbgr32s <const> = #abgr32s
+            palette:resize(lenAbgr32s)
+            local j = 0
+            while j < lenAbgr32s do
+                j = j + 1
+                local aseColor <const> = AseUtilities.hexToAseColor(abgr32s[j])
+                palette:setColor(j - 1, aseColor)
+            end
+        end)
+
+        app.refresh()
+    end
+}
+
+dlg:button {
+    id = "cancel",
+    text = "&CANCEL",
+    focus = false,
+    onclick = function()
+        dlg:close()
+    end
+}
+
+dlg:show {
+    autoscrollbars = true,
+    wait = false
+}
